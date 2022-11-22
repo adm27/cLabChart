35e871e6f15f620519b4eddf88afb444012a1168
adm27 <alexdelmar27@gmail.com>
2022-11-22 21:26:19 +0100

    add requirements to deploy

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,13 @@
-import json
-
+import os
 import pymongo as pymongo
 
 
 def get_database():
     try:
-        file = open("db_config_file.json", "r")
-        db_config = json.load(file)
-        client = pymongo.MongoClient("mongodb+srv://"+db_config['user']+":"+db_config['password']+"@cluster0.dt9r9fo.mongodb.net/?retryWrites=true&w=majority")
+        db_username = os.environ['DB_USERNAME']
+        db_password = os.environ['DB_PASSWORD']
+
+        client = pymongo.MongoClient("mongodb+srv://"+db_username+":"+db_password+"@cluster0.dt9r9fo.mongodb.net/?retryWrites=true&w=majority")
         db = client["circularLabDB"]
         return db
     except Exception as e:
